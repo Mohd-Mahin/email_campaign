bc001f918fa6d195daa600b0d162824776313b03
Mohd-Mahin <mahinit3016@gmail.com>
2024-07-28 17:16:15 +0530

    minor change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send({ hi: "hello" });
+  res.send({ hi: "hello", world: "world" });
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
